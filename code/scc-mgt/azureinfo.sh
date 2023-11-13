8a920699443f6699c3616687a003df975766c44b
RicardoGoncalo <r.goncalo@campus.fct.unl.pt>
2023-11-13 14:23:17 +0000

    Update azureinfo.sh

--- code/scc-mgt/azureinfo.sh
+++ code/scc-mgt/azureinfo.sh
@@ -1,2 +1,7 @@
-az functionapp list --query "[].{name : "name", hostName: defaultHostName, state: state, "rg": "resourceGroup"}"
+echo "list of azure function apps:"
+az functionapp list --query "[].{name : "name", hostName: defaultHostName, state: state, ResourceGroup: "resourceGroup"}"
+echo "list of settings for functions: "
+az functionapp config appsettings list --name scc24funwesteurope60519 --resource-group scc24-rg-westeurope-60519
+echo "list of settings for app: "
+az functionapp config appsettings list --name scc24appwesteurope60519 --resource-group scc24-rg-westeurope-60519
 read
