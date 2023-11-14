60f6bfef21b6f08b7d0e5d217612eb1bfd65b7c8
RicardoGoncalo <r.goncalo@campus.fct.unl.pt>
2023-11-14 22:24:35 +0000

    Media: preparing to allow propagation

--- code/project/src/scc/srv/MediaResource.java
+++ code/project/src/scc/srv/MediaResource.java
@@ -5,6 +5,7 @@ import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobContainerClientBuilder;
 import com.azure.storage.blob.models.BlobItem;
+import jakarta.ws.rs.*;
 import scc.utils.Hash;
 
 import java.util.ArrayList;
@@ -12,14 +13,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.NotFoundException;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.ServiceUnavailableException;
 import jakarta.ws.rs.core.MediaType;
 
 /**
@@ -68,11 +61,46 @@ public class MediaResource
 
 			LogResource.writeLine("   Error uploading: " + e.getMessage());
 
-			e.printStackTrace();
+			throw new InternalServerErrorException("Error uploading");
 		}
 
 		return key;
 
+	}
+
+	/**
+	 * Post a new image that was uploaded in other region.
+	 */
+	@POST
+	@Path("/{id}")
+	@Consumes(MediaType.APPLICATION_OCTET_STREAM)
+	public void propagatedUpload(@PathParam("id") String id, byte[] contents) {
+
+		LogResource.writeLine("MEDIA : PROPAGATED UPLOAD : id = " + id);
+
+		try {
+			BinaryData data = BinaryData.fromBytes(contents);
+
+			// Get container client
+			BlobContainerClient containerClient = new BlobContainerClientBuilder()
+					.connectionString(STORAGE_CONNECT_STRING)
+					.containerName(CONTAINER_NAME)
+					.buildClient();
+
+			// Get client to blob
+			BlobClient blob = containerClient.getBlobClient( id);
+
+			// Upload contents from BinaryData (check documentation for other alternatives)
+			blob.upload(data);
+
+			LogResource.writeLine("    File uploaded");
+
+		} catch( Exception e) {
+
+			LogResource.writeLine("   Error uploading: " + e.getMessage());
+
+			throw new InternalServerErrorException("Error uploading");
+		}
 
 	}
 
