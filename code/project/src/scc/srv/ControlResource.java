17c45d6a662789c2f1623237c33b93f3cd36ea63
RicardoGoncalo <r.goncalo@campus.fct.unl.pt>
2023-11-15 15:41:12 +0000

    Update ControlResource.java

--- code/project/src/scc/srv/ControlResource.java
+++ code/project/src/scc/srv/ControlResource.java
@@ -1,9 +1,13 @@
 package scc.srv;
 
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import scc.db.CosmosDBLayer;
+import scc.cache.RedisCache;
 
 /**
  * Class with control endpoints.
@@ -13,18 +17,25 @@ public class ControlResource
 {
 
 	/**
-	 * This methods just prints a string. It may be useful to check if the current 
+	 * This methods just prints a string. It may be useful to check if the current
 	 * version is running on Azure.
 	 */
 	@Path("/version")
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String hello() {
-
-		LogResource.writeLine("CTRL : GET VERSION");
-
 		return "v: 0001";
-
 	}
+
+	//delete everything in cache and cosmosdb
+	@Path("/reset")
+	@DELETE
+	public Response reset() {
+		CosmosDBLayer.getInstance().reset();
+		RedisCache.reset();
+		LogResource.reset();
+		return Response.ok().build();
+	}
+
 
 }
